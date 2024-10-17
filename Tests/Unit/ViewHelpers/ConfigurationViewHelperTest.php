de159b0f9e84fce62ac1702d027ffaccf74571da
Rafal Jania <rj@studiomitte.com>
2024-10-17 15:41:06 +0200

    [TASK] Improve ConfigurationViewHelper - cleanup, change renderStatic() -> render()

--- Tests/Unit/ViewHelpers/ConfigurationViewHelperTest.php
+++ Tests/Unit/ViewHelpers/ConfigurationViewHelperTest.php
@@ -6,7 +6,6 @@ namespace StudioMitte\FriendlyCaptcha\Tests\Unit\ViewHelpers;
 
 use StudioMitte\FriendlyCaptcha\Tests\RequestTrait;
 use StudioMitte\FriendlyCaptcha\ViewHelpers\ConfigurationViewHelper;
-use TYPO3\CMS\Fluid\Core\Rendering\RenderingContext;
 use TYPO3\TestingFramework\Core\BaseTestCase;
 
 class ConfigurationViewHelperTest extends BaseTestCase
@@ -19,16 +18,14 @@ class ConfigurationViewHelperTest extends BaseTestCase
     public function viewHelperReturnsProperConfiguration()
     {
         self::setupRequest();
-
-        $renderingContext = new class () extends RenderingContext {
-            public function __construct() {}
-        };
+        $configurationViewHelper = new ConfigurationViewHelper();
+        
         self::assertSame([
             'siteKey' => '1234',
             'verifyUrl' => 'https://verify,https://verify2',
             'puzzleEndpoint' => '',
             'jsPath' => 'EXT:friendlycaptcha_official/Resources/Public/JavaScript/lib/sdk@0.1.8-site.compat.min.js',
             'enabled' => true,
-        ], ConfigurationViewHelper::renderStatic([], static fn() => '', $renderingContext));
+        ], $configurationViewHelper->render());
     }
 }
