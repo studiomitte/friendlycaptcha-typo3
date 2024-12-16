7344864102f0c21fa5fd6fda42cbc4a9498cd0eb
Rafal Jania <rj@studiomitte.com>
2024-12-16 09:11:55 +0100

    Return support for typo3 v11

--- Classes/ViewHelpers/ConfigurationViewHelper.php
+++ Classes/ViewHelpers/ConfigurationViewHelper.php
@@ -6,12 +6,9 @@ namespace StudioMitte\FriendlyCaptcha\ViewHelpers;
 
 use StudioMitte\FriendlyCaptcha\Configuration;
 use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
-use TYPO3Fluid\Fluid\Core\ViewHelper\Traits\CompileWithRenderStatic;
 
 class ConfigurationViewHelper extends AbstractViewHelper
 {
-    use CompileWithRenderStatic;
-
     public function render(): array
     {
         $configuration = new Configuration();
