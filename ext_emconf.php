0d6d229aa64f7afdf98af0e35052107b44c1a581
Georg Ringer <georg.ringer@gmail.com>
2025-02-20 11:14:25 +0100

    [TASK] Readd extemconf

---
+++ ext_emconf.php
@@ -0,0 +1,21 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Integration of Friendly Captcha',
+    'description' => 'FriendlyCaptcha Integration for EXT:powermail and EXT:form and your custom implementation',
+    'category' => 'plugin',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '12.4.5-13.4.99',
+        ],
+        'conflicts' => [
+        ],
+    ],
+    'autoload' => [
+        'psr-4' => [
+            'StudioMitte\\FriendlyCaptcha\\' => 'Classes',
+        ],
+    ],
+    'state' => 'stable',
+    'version' => '2.0.0',
+];
